ce2244ba52f597083027a79b7daa52a9f39fb8b2
Ghaith Chrit <ghaithfinadnan@gmail.com>
2023-09-15 14:38:45 -0400

    #3 Add backend skeleton

---
+++ Server/ConcordiaCurriculumManager/DTO/AuthenticationResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ConcordiaCurriculumManager.DTO;
+
+public class AuthenticationResponse
+{
+    public const string MediaType = "application/x.ccm.authentication.token+json;v=1";
+
+    public required string AccessToken { get; set; }
+}
+
