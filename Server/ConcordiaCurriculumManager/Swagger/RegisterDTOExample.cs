bd68785b1f69f2118150ff0012dd2eb5292e546b
Ghaith Chrit <ghaithfinadnan@gmail.com>
2023-09-15 14:40:58 -0400

    #3 Add Swagger Examples

---
+++ Server/ConcordiaCurriculumManager/Swagger/RegisterDTOExample.cs
@@ -0,0 +1,18 @@
+﻿using ConcordiaCurriculumManager.DTO;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace ConcordiaCurriculumManager.Swagger;
+
+public class RegisterDTOExample : IExamplesProvider<RegisterDTO>
+{
+    public RegisterDTO GetExamples()
+    {
+        return new()
+        {
+            FirstName = "FirstNameExample",
+            LastName = "LastNameExample",
+            Email = "EmailExample@example.com",
+            Password = "ExamplePassword123!",
+        };
+    }
+}
