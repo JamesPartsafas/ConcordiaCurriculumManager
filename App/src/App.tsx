dd6b9d4f1a438d92f9e2c470f00bcd25712b9a87
Marc-Eid <marc.mn.eid@gmail.com>
2023-09-21 11:23:42 -0400

    #29 added basic login page and chakraui

--- App/src/App.tsx
+++ App/src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react";
+
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
 
 export function App() {
     return (
         <>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
                 {/* whenever none of the other routes match we show the not found page */}
                 <Route path="*" element={<NotFound />} />
             </Routes>
@@ -17,7 +23,9 @@ export function App() {
 export function WrappedApp() {
     return (
         <BrowserRouter>
-            <App />
+            <ChakraProvider>
+                <App />
+            </ChakraProvider>
         </BrowserRouter>
     );
 }
---
+++ App/src/pages/Login.tsx
@@ -0,0 +1,13 @@
+import { Card, CardBody, Text } from "@chakra-ui/react";
+
+export default function Login() {
+    return (
+        <>
+            <Card>
+                <CardBody>
+                    <Text>this is a login page</Text>
+                </CardBody>
+            </Card>
+        </>
+    );
+}
