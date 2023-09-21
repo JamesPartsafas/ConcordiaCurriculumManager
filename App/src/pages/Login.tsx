ac86a2ffb3da59808e77fe10385babd27f9d0d1c
Marc-Eid <marc.mn.eid@gmail.com>
2023-09-21 15:49:06 -0400

    #29 added some form validation

--- App/src/pages/Login.tsx
+++ App/src/pages/Login.tsx
@@ -15,82 +15,114 @@ import {
 } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { LoginDTO } from "../services/auth";
 
 export default function Login() {
+    const { register, handleSubmit } = useForm<LoginDTO>();
     const [showPassword, setShowPassword] = useState(false);
 
     function toggleShowPassword() {
         setShowPassword(!showPassword);
     }
 
+    function onSubmit(data: unknown) {
+        console.log(data);
+    }
     return (
         <>
-            <Container
-                maxW="lg"
-                py={{ base: "12", md: "24" }}
-                px={{ base: "0", sm: "8" }}
-            >
-                <Stack spacing="8">
-                    <Stack spacing="6">
-                        <Image
-                            src={logo}
-                            width="50px"
-                            height="50px"
-                            margin="auto"
-                        />
-                        <Heading textAlign="center" size="lg">
-                            Log in to your account
-                        </Heading>
-                    </Stack>
-                    <Box
-                        py={{ base: "0", sm: "8" }}
-                        px={{ base: "4", sm: "10" }}
-                        bg={{ base: "transparent", sm: "bg.surface" }}
-                        boxShadow={{ base: "none", sm: "md" }}
-                        borderRadius={{ base: "none", sm: "xl" }}
-                    >
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Container
+                    maxW="lg"
+                    py={{ base: "12", md: "24" }}
+                    px={{ base: "0", sm: "8" }}
+                >
+                    <Stack spacing="8">
                         <Stack spacing="6">
-                            <Stack spacing="5">
-                                <FormControl>
-                                    <FormLabel htmlFor="email">Email</FormLabel>
-                                    <Input id="email" type="email" />
-                                    <FormLabel htmlFor="password">
-                                        Password
-                                    </FormLabel>
-                                    <InputGroup>
-                                        <Input
-                                            id="password"
-                                            type={
-                                                showPassword
-                                                    ? "text"
-                                                    : "password"
-                                            }
-                                        />
-                                        <InputRightElement width="4.5rem">
-                                            <Button
-                                                h="1.75rem"
-                                                size="sm"
-                                                onClick={toggleShowPassword}
-                                            >
-                                                {showPassword ? "Hide" : "Show"}
-                                            </Button>
-                                        </InputRightElement>
-                                    </InputGroup>
-                                </FormControl>
-                            </Stack>
-                            <HStack justify="space-between">
-                                <Checkbox defaultChecked>Remember me</Checkbox>
-                                <Button variant="text" size="sm">
-                                    Forgot password?
-                                </Button>
-                            </HStack>
-                            <Stack spacing="6">
-                                <Button>Sign in</Button>
-                            </Stack>
+                            <Image
+                                src={logo}
+                                width="50px"
+                                height="50px"
+                                margin="auto"
+                            />
+                            <Heading textAlign="center" size="lg">
+                                Log in to your account
+                            </Heading>
                         </Stack>
-                    </Box>
-                </Stack>
-            </Container>
+                        <Box
+                            py={{ base: "0", sm: "8" }}
+                            px={{ base: "4", sm: "10" }}
+                            bg={{ base: "transparent", sm: "bg.surface" }}
+                            boxShadow={{ base: "none", sm: "2xl" }}
+                            borderRadius={{ base: "none", sm: "xl" }}
+                        >
+                            <Stack spacing="6">
+                                <Stack spacing="5">
+                                    <FormControl>
+                                        <FormLabel htmlFor="email">
+                                            Email
+                                        </FormLabel>
+                                        <Input
+                                            id="email"
+                                            type="email"
+                                            {...register("email", {
+                                                required: true,
+                                                pattern:
+                                                    /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                            })}
+                                        />
+                                        <FormLabel htmlFor="password">
+                                            Password
+                                        </FormLabel>
+                                        <InputGroup>
+                                            <Input
+                                                id="password"
+                                                type={
+                                                    showPassword
+                                                        ? "text"
+                                                        : "password"
+                                                }
+                                                {...register("password", {
+                                                    required: true,
+                                                })}
+                                            />
+                                            <InputRightElement width="4.5rem">
+                                                <Button
+                                                    h="1.75rem"
+                                                    size="sm"
+                                                    onClick={toggleShowPassword}
+                                                >
+                                                    {showPassword
+                                                        ? "Hide"
+                                                        : "Show"}
+                                                </Button>
+                                            </InputRightElement>
+                                        </InputGroup>
+                                    </FormControl>
+                                </Stack>
+                                <HStack justify="space-between">
+                                    <Checkbox defaultChecked>
+                                        Remember me
+                                    </Checkbox>
+                                    <Button variant="text" size="sm">
+                                        Forgot password?
+                                    </Button>
+                                </HStack>
+                                <Stack spacing="6">
+                                    <Button
+                                        backgroundColor="#932439"
+                                        color="white"
+                                        _hover={{ bg: "#7A1D2E" }}
+                                        type="submit"
+                                    >
+                                        Sign in
+                                    </Button>
+                                </Stack>
+                            </Stack>
+                        </Box>
+                    </Stack>
+                </Container>
+            </form>
         </>
     );
 }
