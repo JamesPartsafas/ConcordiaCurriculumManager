015cbea46c5b03d1f10d93ea1f72a236618c4a2d
Marc-Eid <marc.mn.eid@gmail.com>
2023-09-21 14:48:41 -0400

    added all frontend components to the login page #29

--- App/src/pages/Login.tsx
+++ App/src/pages/Login.tsx
@@ -1,13 +1,96 @@
-import { Card, CardBody, Text } from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    Checkbox,
+    Container,
+    FormControl,
+    FormLabel,
+    Heading,
+    HStack,
+    Image,
+    Input,
+    InputGroup,
+    InputRightElement,
+    Stack,
+} from "@chakra-ui/react";
+import logo from "../assets/logo.png";
+import { useState } from "react";
 
 export default function Login() {
+    const [showPassword, setShowPassword] = useState(false);
+
+    function toggleShowPassword() {
+        setShowPassword(!showPassword);
+    }
+
     return (
         <>
-            <Card>
-                <CardBody>
-                    <Text>this is a login page</Text>
-                </CardBody>
-            </Card>
+            <Container
+                maxW="lg"
+                py={{ base: "12", md: "24" }}
+                px={{ base: "0", sm: "8" }}
+            >
+                <Stack spacing="8">
+                    <Stack spacing="6">
+                        <Image
+                            src={logo}
+                            width="50px"
+                            height="50px"
+                            margin="auto"
+                        />
+                        <Heading textAlign="center" size="lg">
+                            Log in to your account
+                        </Heading>
+                    </Stack>
+                    <Box
+                        py={{ base: "0", sm: "8" }}
+                        px={{ base: "4", sm: "10" }}
+                        bg={{ base: "transparent", sm: "bg.surface" }}
+                        boxShadow={{ base: "none", sm: "md" }}
+                        borderRadius={{ base: "none", sm: "xl" }}
+                    >
+                        <Stack spacing="6">
+                            <Stack spacing="5">
+                                <FormControl>
+                                    <FormLabel htmlFor="email">Email</FormLabel>
+                                    <Input id="email" type="email" />
+                                    <FormLabel htmlFor="password">
+                                        Password
+                                    </FormLabel>
+                                    <InputGroup>
+                                        <Input
+                                            id="password"
+                                            type={
+                                                showPassword
+                                                    ? "text"
+                                                    : "password"
+                                            }
+                                        />
+                                        <InputRightElement width="4.5rem">
+                                            <Button
+                                                h="1.75rem"
+                                                size="sm"
+                                                onClick={toggleShowPassword}
+                                            >
+                                                {showPassword ? "Hide" : "Show"}
+                                            </Button>
+                                        </InputRightElement>
+                                    </InputGroup>
+                                </FormControl>
+                            </Stack>
+                            <HStack justify="space-between">
+                                <Checkbox defaultChecked>Remember me</Checkbox>
+                                <Button variant="text" size="sm">
+                                    Forgot password?
+                                </Button>
+                            </HStack>
+                            <Stack spacing="6">
+                                <Button>Sign in</Button>
+                            </Stack>
+                        </Stack>
+                    </Box>
+                </Stack>
+            </Container>
         </>
     );
 }
