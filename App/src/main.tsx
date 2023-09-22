738991982d357b47991bb58fe32dfe2a56f21be3
Marc-Eid <marc.mn.eid@gmail.com>
2023-09-22 18:43:12 -0400

    #29 removed commented line

--- App/src/main.tsx
+++ App/src/main.tsx
@@ -4,7 +4,6 @@ import { WrappedApp } from "./App.tsx";
 import "./index.css";
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://localhost:7282";
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
