f2482baf18c43823ae3cee91c57d113eaeab2ec3
Marc-Eid <marc.mn.eid@gmail.com>
2023-09-21 23:49:25 -0400

    #29fixed routing domain to send to backend

--- App/src/main.tsx
+++ App/src/main.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { WrappedApp } from "./App.tsx";
 import "./index.css";
+import axios from "axios";
+
+// axios.defaults.baseURL = "https://localhost:7282";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
