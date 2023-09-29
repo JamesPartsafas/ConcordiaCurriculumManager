909560482060f68344006f42cea02180d2ed3606
Ghaith Chrit <ghaithfinadnan@gmail.com>
2023-09-29 17:01:27 -0400

    Add sample test case #54

---
+++ App/__tests__/Registration.test.tsx
@@ -0,0 +1,20 @@
+import Register from "../src/pages/Register";
+import { render } from "@testing-library/react";
+import { BrowserRouter } from 'react-router-dom';
+
+describe("Registration Test Case", () => {
+    it("validate function should render all elements ", () => {
+        const component = render(
+            <BrowserRouter>
+                <Register
+                    setUser={() => {
+                        throw new Error("Function not implemented.");
+                    }}
+                />
+            </BrowserRouter>
+        );
+
+        const labelNode = component.getByText("Email");
+        expect(labelNode).toBeDefined;
+    });
+});
