0684a87fe3b485448cc75d2c6467dac2d565b380
Ghaith Chrit <ghaithfinadnan@gmail.com>
2023-09-29 17:01:17 -0400

    Setup frontend infrastructure #54

---
+++ App/jest.config.ts
@@ -0,0 +1,18 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  testMatch: ["**/***.test.tsx"],
+  moduleFileExtensions: [
+    "js",
+    "jsx",
+    "tsx",
+    "ts"
+  ],
+  rootDir: "__tests__/",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/image_mock.tsx'
+  },
+}
