3c9aafa7c6504bbc962ca754e98c0082905b4bd7
Marc-Eid <marc.mn.eid@gmail.com>
2023-10-09 19:09:47 -0400

    added alert to delete dossir button #69

--- App/src/pages/dossier/Dossiers.tsx
+++ App/src/pages/dossier/Dossiers.tsx
@@ -1,10 +1,29 @@
-import { Table, Thead, Tbody, Text, Tr, Th, Td, TableContainer, IconButton } from "@chakra-ui/react";
+import {
+    Table,
+    Thead,
+    Tbody,
+    Text,
+    Tr,
+    Th,
+    Td,
+    TableContainer,
+    IconButton,
+    AlertDialog,
+    AlertDialogOverlay,
+    AlertDialogContent,
+    AlertDialogHeader,
+    AlertDialogBody,
+    AlertDialogFooter,
+    Button,
+    useDisclosure,
+} from "@chakra-ui/react";
 import { DeleteIcon, EditIcon, InfoIcon } from "@chakra-ui/icons";
 
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../App";
 import { DossierDTO, GetMyDossiersResponse, getMyDossiers } from "../../services/dossier";
 import EditDossierModal from "./EditDossierModal";
+import React from "react";
 
 export default function Dossiers() {
     const user = useContext(UserContext);
@@ -12,6 +31,9 @@ export default function Dossiers() {
     const [myDossiers, setMyDossiers] = useState<DossierDTO[]>([]);
     const [showEditDossierModal, setShowEditDossierModal] = useState<boolean>(false);
     const [selectedDossier, setSelectedDossier] = useState<DossierDTO | null>(null);
+
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const cancelRef = React.useRef();
 
     useEffect(() => {
         getAllDossiers();
@@ -32,13 +54,52 @@ export default function Dossiers() {
             });
     }
 
-    function displayEditDossierModal(dossier: DossierDTO) {
-        setSelectedDossier(dossier);
+    // endpoint not implemented yet
+    function deleteDossier(dossier: DossierDTO) {
+        console.log(dossier);
+    }
+
+    function displayEditDossierModal() {
         setShowEditDossierModal(true);
     }
 
     function closeEditDossierModal() {
         setShowEditDossierModal(false);
+    }
+
+    function deleteAlertDialog() {
+        return (
+            <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
+                <AlertDialogOverlay>
+                    <AlertDialogContent>
+                        <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                            Delete Dossier
+                        </AlertDialogHeader>
+
+                        <AlertDialogBody>
+                            Are you sure you want to delete <b>{selectedDossier.title}</b>? You can't undo this action
+                            afterwards.
+                        </AlertDialogBody>
+
+                        <AlertDialogFooter>
+                            <Button ref={cancelRef} onClick={onClose}>
+                                Cancel
+                            </Button>
+                            <Button
+                                colorScheme="red"
+                                onClick={() => {
+                                    deleteDossier(selectedDossier);
+                                    onClose();
+                                }}
+                                ml={3}
+                            >
+                                Delete
+                            </Button>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialogOverlay>
+            </AlertDialog>
+        );
     }
 
     return (
@@ -73,7 +134,10 @@ export default function Dossiers() {
                                         aria-label="Delete"
                                         icon={<DeleteIcon />}
                                         backgroundColor={"#932439"}
-                                        onClick={() => console.log("delete")}
+                                        onClick={() => {
+                                            setSelectedDossier(dossier);
+                                            onOpen();
+                                        }}
                                     />
                                     <IconButton
                                         ml={2}
@@ -81,7 +145,8 @@ export default function Dossiers() {
                                         icon={<EditIcon />}
                                         backgroundColor={"#0072a8"}
                                         onClick={() => {
-                                            displayEditDossierModal(dossier);
+                                            setSelectedDossier(dossier);
+                                            displayEditDossierModal();
                                         }}
                                     />
                                     <IconButton
@@ -96,6 +161,7 @@ export default function Dossiers() {
                     </Tbody>
                 </Table>
             </TableContainer>
+            {deleteAlertDialog()}
             {/* this is the Dossier Modal */}
             {showEditDossierModal && (
                 <EditDossierModal
