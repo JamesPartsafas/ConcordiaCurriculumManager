ab1a027f0634b53d201ed1bbf1dc96592c8bc46f
Tony Yang <yangtony2000@gmail.com>
2023-10-11 16:29:16 -0400

    added requested changes

--- Server/ConcordiaCurriculumManager/Controllers/GroupController.cs
+++ Server/ConcordiaCurriculumManager/Controllers/GroupController.cs
@@ -2,11 +2,14 @@
 using ConcordiaCurriculumManager.Models.Users;
 using ConcordiaCurriculumManager.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace ConcordiaCurriculumManager.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
+[Authorize]
 
 public class GroupController : Controller
 {
@@ -18,6 +21,9 @@ public class GroupController : Controller
     }
 
     [HttpPost]
+    [Authorize(Roles = RoleNames.Admin)]
+    [SwaggerResponse(StatusCodes.Status201Created, "Group created successfully")]
+    [SwaggerResponse(StatusCodes.Status500InternalServerError, "An error occured while creating the group")]
     public async Task<IActionResult> CreateGroup([FromBody] GroupCreateDTO groupCreateDTO)
     {
         var group = new Group { Name = groupCreateDTO.Name };
@@ -34,6 +40,8 @@ public class GroupController : Controller
     }
 
     [HttpGet("{id}")]
+    [SwaggerResponse(StatusCodes.Status200OK, "Group retrieved successfully")]
+    [SwaggerResponse(StatusCodes.Status404NotFound, "Group not found")]
     public async Task<IActionResult> GetGroupById(Guid id)
     {
         var group = await _groupService.GetGroupByIdAsync(id);
@@ -45,6 +53,9 @@ public class GroupController : Controller
     }
 
     [HttpPost("{groupId}/users/{userId}")]
+    [Authorize(Roles = RoleNames.Admin)]
+    [SwaggerResponse(StatusCodes.Status200OK, "User added to group successfully")]
+    [SwaggerResponse(StatusCodes.Status404NotFound, "Error adding user to group")]
     public async Task<IActionResult> AddUserToGroup(Guid groupId, Guid userId)
     {
         var result = await _groupService.AddUserToGroup(userId, groupId);
@@ -56,6 +67,9 @@ public class GroupController : Controller
     }
 
     [HttpDelete("{groupId}/users/{userId}")]
+    [Authorize(Roles = RoleNames.Admin)]
+    [SwaggerResponse(StatusCodes.Status200OK, "User removed from group successfully")]
+    [SwaggerResponse(StatusCodes.Status404NotFound, "Error removing user from group")]
     public async Task<IActionResult> RemoveUserFromGroup(Guid groupId, Guid userId)
     {
         var result = await _groupService.RemoveUserFromGroup(userId, groupId);
--- Server/ConcordiaCurriculumManager/Services/GroupService.cs
+++ Server/ConcordiaCurriculumManager/Services/GroupService.cs
@@ -1,11 +1,13 @@
-﻿using ConcordiaCurriculumManager.Models.Users;
+﻿using AutoMapper;
+using ConcordiaCurriculumManager.Models.Users;
 using ConcordiaCurriculumManager.Repositories;
+using ConcordiaCurriculumManager.DTO;
 
 namespace ConcordiaCurriculumManager.Services;
 
 public interface IGroupService
 {
-    Task<Group?> GetGroupByIdAsync(Guid id);
+    Task<GroupDTO?> GetGroupByIdAsync(Guid id);
     Task<bool> CreateGroupAsync(Group group);
     Task<bool> AddUserToGroup(Guid userId, Guid groupId);
     Task<bool> RemoveUserFromGroup(Guid userId, Guid groupId);
@@ -14,10 +16,12 @@ public interface IGroupService
 public class GroupService : IGroupService
 {
     private readonly IGroupRepository _groupRepository;
+    private readonly IMapper _mapper;
 
-    public GroupService(IGroupRepository groupRepository)
+    public GroupService(IGroupRepository groupRepository, IMapper mapper)
     {
         _groupRepository = groupRepository;
+        _mapper = mapper;
     }
 
     public async Task<bool> CreateGroupAsync(Group group)
@@ -25,9 +29,17 @@ public class GroupService : IGroupService
         return await _groupRepository.SaveGroup(group);
     }
 
-    public async Task<Group?> GetGroupByIdAsync(Guid id)
+    public async Task<GroupDTO?> GetGroupByIdAsync(Guid id)
     {
-        return await _groupRepository.GetGroupById(id);
+        var group = await _groupRepository.GetGroupById(id);
+        if (group == null)
+        {
+            return null;
+        }
+        else
+        {
+            return _mapper.Map<GroupDTO>(group);
+        }
     }
 
     public async Task<bool> AddUserToGroup(Guid userId, Guid groupId)
