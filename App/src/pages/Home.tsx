1443ac77d3257aadf2c3caf2670e8545e88ee263
DanielSoldera <d.v.soldera@gmail.com>
2023-10-16 10:24:03 -0400

    home page modification

--- App/src/pages/Home.tsx
+++ App/src/pages/Home.tsx
@@ -1,7 +1,34 @@
 import { useContext } from "react";
 import { UserContext } from "../App";
+import Button from "../components/Button";
 
 export default function Home() {
     const user = useContext(UserContext);
-    return <h1>Hello {user?.firstName}</h1>;
+    return (
+        <div>
+            <h1
+                style={{
+                    margin: "5%",
+                    fontWeight: "bold",
+                    fontSize: "24px",
+                    color: "#FF8888",
+                    textAlign: "center",
+                }}
+            >
+                Hello {user?.firstName}, Welcome to the Concordia Curriculum Manager!
+            </h1>
+
+            <h2
+                style={{
+                    marginBottom: "20px",
+                    fontWeight: "bold",
+                    fontSize: "24px",
+                    color: "#FF8888",
+                    textAlign: "center",
+                }}
+            >
+                Dossier List: <Button type="primary"></Button>
+            </h2>
+        </div>
+    );
 }
