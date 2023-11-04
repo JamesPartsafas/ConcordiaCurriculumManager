3f84393e1bbe63d7421bf38f0a1ab9b2652bae45
Kenny Phan <kennyphan100@gmail.com>
2023-11-04 11:33:32 -0400

    #145 Implement frontend UI page for course deletion request

--- App/src/services/course.ts
+++ App/src/services/course.ts
@@ -8,6 +8,7 @@ interface GetAllCourseSettingsResponse {
 const CourseAPIEndpoints = {
     GetAllCourseSettings: "/Course/GetAllCourseSettings",
     AddCourse: "/Course/InitiateCourseCreation",
+    DeleteCourse: "/Course/InitiateCourseDeletion",
 };
 
 export function getAllCourseSettings(): Promise<GetAllCourseSettingsResponse> {
@@ -16,4 +17,8 @@ export function getAllCourseSettings(): Promise<GetAllCourseSettingsResponse> {
 
 export function addCourse(course: Course): Promise<unknown> {
     return axios.post(CourseAPIEndpoints.AddCourse, course);
+}
+
+export function deleteCourse(courseDeletionRequest): Promise<unknown> {
+    return axios.post(CourseAPIEndpoints.DeleteCourse, courseDeletionRequest);
 }
