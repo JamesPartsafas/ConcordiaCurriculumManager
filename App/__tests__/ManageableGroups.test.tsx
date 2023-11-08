523c6ff1b894f2400eecdfaa328033bb68983a02
DanielSoldera <d.v.soldera@gmail.com>
2023-11-08 16:23:15 -0500

    Fix all rendering tests

--- App/__tests__/ManageableGroups.test.tsx
+++ App/__tests__/ManageableGroups.test.tsx
@@ -1,13 +1,30 @@
 import { render } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import DisplayManageableGroups from "../src/pages/ManageableGroups";
+import { UserContext } from "../src/App";
+import { User } from "../src/models/user";
 
 describe("Add User to Group Test Case", () => {
     it("validate function should render all elements ", () => {
+        const user: User = {
+            firstName: "test",
+            lastName: "testing",
+            id: "204658362",
+            email: "jest@gmail.com",
+            issuedAtTimestamp: Date.now(),
+            expiresAtTimestamp: 100000000000000,
+            roles: ["Admin"],
+            groups: [],
+            masteredGroups: [],
+            issuer: "admin",
+            audience: "",
+        };
         const component = render(
-            <BrowserRouter>
-                <DisplayManageableGroups />
-            </BrowserRouter>
+            <UserContext.Provider value={user}>
+                <BrowserRouter>
+                    <DisplayManageableGroups />
+                </BrowserRouter>
+            </UserContext.Provider>
         );
 
         const labelNode = component.getByText("Group Name");
