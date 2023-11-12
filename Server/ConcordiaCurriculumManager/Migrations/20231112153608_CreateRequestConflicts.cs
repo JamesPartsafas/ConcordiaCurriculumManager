baefc82315ff6f11d8ba0bca033a6bfc07e114a0
JamesThePlant <56362880+JamesThePlant@users.noreply.github.com>
2023-11-12 10:46:09 -0500

    #191 Generate migration

---
+++ Server/ConcordiaCurriculumManager/Migrations/20231112153608_CreateRequestConflicts.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConcordiaCurriculumManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateRequestConflicts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "CourseModificationRequests",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Conflict",
+                table: "CourseModificationRequests",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "CourseDeletionRequests",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Conflict",
+                table: "CourseDeletionRequests",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "CourseCreationRequests",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Conflict",
+                table: "CourseCreationRequests",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Conflict",
+                table: "CourseModificationRequests");
+
+            migrationBuilder.DropColumn(
+                name: "Conflict",
+                table: "CourseDeletionRequests");
+
+            migrationBuilder.DropColumn(
+                name: "Conflict",
+                table: "CourseCreationRequests");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "CourseModificationRequests",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "CourseDeletionRequests",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "CourseCreationRequests",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
