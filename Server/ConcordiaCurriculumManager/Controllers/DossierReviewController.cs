2fcc2213f7f647876222e3c20a50ea35220feace
JamesThePlant <56362880+JamesThePlant@users.noreply.github.com>
2023-11-13 15:40:56 -0500

    #195 Create endpoint for dossier submission

---
+++ Server/ConcordiaCurriculumManager/Controllers/DossierReviewController.cs
@@ -0,0 +1,34 @@
+﻿using ConcordiaCurriculumManager.DTO.Dossiers.DossierReview;
+using ConcordiaCurriculumManager.Security;
+using ConcordiaCurriculumManager.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConcordiaCurriculumManager.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Authorize]
+public class DossierReviewController : Controller
+{
+    private readonly IDossierReviewService _dossierReviewService;
+
+    public DossierReviewController(IDossierReviewService dossierReviewService)
+    {
+        _dossierReviewService = dossierReviewService;
+    }
+
+    [HttpPost(nameof(SubmitDossierForReview) + "/{dossierId}")]
+    [Authorize(Policies.IsOwnerOfDossier)]
+    [SwaggerResponse(StatusCodes.Status422UnprocessableEntity, "Invalid input")]
+    [SwaggerResponse(StatusCodes.Status500InternalServerError, "Unexpected error")]
+    [SwaggerResponse(StatusCodes.Status200OK, "Dossier successfully submitted for review")]
+    public async Task<ActionResult> SubmitDossierForReview([FromBody, Required] DossierSubmissionDTO dto)
+    {
+        await _dossierReviewService.SubmitDossierForReview(dto);
+
+        return Ok();
+    }
+}
---
+++ Server/ConcordiaCurriculumManager/DTO/Dossiers/DossierReview/DossierSubmissionDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ConcordiaCurriculumManager.DTO.Dossiers.DossierReview;
+
+public class DossierSubmissionDTO
+{
+    public required Guid DossierId { get; set; }
+
+    public required IList<Guid> GroupIds { get; set; }
+}
