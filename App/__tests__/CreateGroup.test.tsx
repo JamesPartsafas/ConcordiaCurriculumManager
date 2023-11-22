d2d14c81a0c885cb6469196fb21ccea6bb5e876c
eyadsw <76524254+eyadsw@users.noreply.github.com>
2023-11-22 18:05:31 -0500

    fixing the test

--- App/__tests__/CreateGroup.test.tsx
+++ App/__tests__/CreateGroup.test.tsx
@@ -3,14 +3,25 @@ import { render } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 
 describe("Create Group Test Case", () => {
-    it("validate function should render elements ", () => {
+    it("validate function should render elements", () => {
         const component = render(
             <BrowserRouter>
                 <CreateGroup />
             </BrowserRouter>
         );
 
-        const labelNode = component.getByText("Group Type");
-        expect(labelNode).toBeDefined;
+        // Use a custom function to find the label element
+        const labelNode = component.getByText((content, element) => {
+            // Check if the element has the expected class
+            const hasExpectedClass = element.classList.contains("chakra-form__label");
+
+            // Check if the content includes "Name"
+            const includesExpectedText = content.includes("Name");
+
+            // Return true if both conditions are met
+            return hasExpectedClass && includesExpectedText;
+        });
+
+        expect(labelNode).toBeDefined();
     });
 });
