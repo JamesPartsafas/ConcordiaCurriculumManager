f1beb02776e7cf23eb6ca2044e48ff37fcbecbe7
JamesThePlant <56362880+JamesThePlant@users.noreply.github.com>
2023-12-09 13:18:15 -0500

    #239 Create dossier forwarding service layer methods

--- Server/ConcordiaCurriculumManager/Services/DossierReviewService.cs
+++ Server/ConcordiaCurriculumManager/Services/DossierReviewService.cs
@@ -1,7 +1,6 @@
 ﻿using ConcordiaCurriculumManager.DTO.Dossiers.DossierReview;
 using ConcordiaCurriculumManager.Filters.Exceptions;
 using ConcordiaCurriculumManager.Models.Curriculum.Dossiers;
-using ConcordiaCurriculumManager.Models.Curriculum.Dossiers.DossierReview;
 using ConcordiaCurriculumManager.Repositories;
 
 namespace ConcordiaCurriculumManager.Services;
@@ -10,7 +9,9 @@ public interface IDossierReviewService
 {
     public Task SubmitDossierForReview(DossierSubmissionDTO dto);
     public Task RejectDossier(Guid dossierId);
+    public Task ForwardDossier(Guid dossierId);
     public Task<Dossier> GetDossierWithApprovalStagesOrThrow(Guid dossierId);
+    public Task<Dossier> GetDossierWithApprovalStagesAndRequestsOrThrow(Guid dossierId);
 }
 
 public class DossierReviewService : IDossierReviewService
@@ -18,6 +19,7 @@ public class DossierReviewService : IDossierReviewService
     private readonly ILogger<DossierReviewService> _logger;
     private readonly IDossierService _dossierService;
     private readonly IGroupService _groupService;
+    private readonly ICourseService _courseService;
     private readonly IDossierRepository _dossierRepository;
     private readonly IDossierReviewRepository _dossierReviewRepository;
 
@@ -25,12 +27,14 @@ public class DossierReviewService : IDossierReviewService
         ILogger<DossierReviewService> logger,
         IDossierService dossierService,
         IGroupService groupService,
+        ICourseService courseService,
         IDossierRepository dossierRepository,
         IDossierReviewRepository dossierReviewRepository)
     {
         _logger = logger;
         _dossierService = dossierService;
         _groupService = groupService;
+        _courseService = courseService;
         _dossierRepository = dossierRepository;
         _dossierReviewRepository = dossierReviewRepository;
     }
@@ -65,6 +69,44 @@ public class DossierReviewService : IDossierReviewService
             _logger.LogError($"Encountered error attempting to reject dossier {dossier.Id} from the review process");
     }
 
+    public async Task ForwardDossier(Guid dossierId)
+    {
+        Dossier dossier = await GetDossierWithApprovalStagesAndRequestsOrThrow(dossierId);
+
+        if (dossier.IsInFinalStageOfReviewPipeline())
+            await AcceptDossierChanges(dossier);
+        else
+            await ForwardDossierToNextGroup(dossier);
+    }
+
+    private async Task ForwardDossierToNextGroup(Dossier dossier)
+    {
+        dossier.MarkAsForwarded();
+
+        var isDossierSaved = await _dossierRepository.UpdateDossier(dossier);
+        if (isDossierSaved)
+            _logger.LogInformation($"Dossier {dossier.Id} successfully forwarded to the next group in the review process");
+        else
+            _logger.LogError($"Encountered error attempting to forward dossier {dossier.Id} to the next group in the review process");
+    }
+
+    private async Task AcceptDossierChanges(Dossier dossier)
+    {
+        var courseVersions = await _courseService.GetCourseVersions(dossier);
+
+        dossier.MarkAsAccepted(courseVersions);
+
+        var isDossierSaved = await _dossierRepository.UpdateDossier(dossier);
+        if (isDossierSaved)
+            _logger.LogInformation($"Dossier {dossier.Id} successfully accepted and removed from the review process");
+        else
+            _logger.LogError($"Encountered error attempting to accept dossier {dossier.Id} and remove it from the review process");
+    }
+
     public async Task<Dossier> GetDossierWithApprovalStagesOrThrow(Guid dossierId) => await _dossierReviewRepository.GetDossierWithApprovalStages(dossierId)
+        ?? throw new NotFoundException("The dossier does not exist.");
+
+    public async Task<Dossier> GetDossierWithApprovalStagesAndRequestsOrThrow(Guid dossierId) => 
+        await _dossierReviewRepository.GetDossierWithApprovalStagesAndRequests(dossierId)
         ?? throw new NotFoundException("The dossier does not exist.");
 }
