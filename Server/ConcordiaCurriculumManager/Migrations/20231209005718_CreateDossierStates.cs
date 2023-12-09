5ab1cac3728946670d789376d3dbf6890eb4945c
JamesThePlant <56362880+JamesThePlant@users.noreply.github.com>
2023-12-08 21:41:50 -0500

    #237 Change published boolean to dossier state enum

---
+++ Server/ConcordiaCurriculumManager/Migrations/20231209005718_CreateDossierStates.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConcordiaCurriculumManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDossierStates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Published",
+                table: "Dossiers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                table: "Dossiers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Dossiers");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Published",
+                table: "Dossiers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
