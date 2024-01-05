2605748adec5340f90d10cec4a6762e68ce1ad29
JamesThePlant <56362880+JamesThePlant@users.noreply.github.com>
2024-01-05 00:18:15 -0500

    #277 create endpoint to query course groupings

---
+++ Server/ConcordiaCurriculumManager/Controllers/CourseGroupingController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using ConcordiaCurriculumManager.DTO.CourseGrouping;
+using ConcordiaCurriculumManager.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConcordiaCurriculumManager.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Authorize]
+public class CourseGroupingController : Controller
+{
+    private readonly IMapper _mapper;
+    private readonly ICourseGroupingService _courseGroupingService;
+
+    public CourseGroupingController(ICourseGroupingService courseGroupingService, IMapper mapper)
+    {
+        _courseGroupingService = courseGroupingService;
+        _mapper = mapper;
+    }
+
+    [HttpGet(nameof(GetCourseGrouping) + "/{courseGroupingId}")]
+    [SwaggerResponse(StatusCodes.Status200OK, "Course grouping data retrieved")]
+    [SwaggerResponse(StatusCodes.Status401Unauthorized, "User is not authorized")]
+    [SwaggerResponse(StatusCodes.Status500InternalServerError, "Unexpected error")]
+    public async Task<ActionResult> GetCourseGrouping([FromRoute, Required] Guid courseGroupingId)
+    {
+        var courseGrouping = await _courseGroupingService.GetCourseGrouping(courseGroupingId);
+        var courseGroupingDTO = _mapper.Map<CourseGroupingDTO>(courseGrouping);
+
+        return Ok(courseGroupingDTO);
+    }
+}
---
+++ Server/ConcordiaCurriculumManager/DTO/CourseGrouping/CourseGroupingDTO.cs
@@ -0,0 +1,23 @@
+﻿using ConcordiaCurriculumManager.DTO.Courses;
+using ConcordiaCurriculumManager.Models.Curriculum.CourseGrouping;
+
+namespace ConcordiaCurriculumManager.DTO.CourseGrouping;
+
+public class CourseGroupingDTO
+{
+    public Guid Id { get; set; }
+    public required Guid CommonIdentifier { get; set; }
+    public required string Name { get; set; }
+    public required string RequiredCredits { get; set; }
+    public required bool IsTopLevel { get; set; }
+    public required SchoolEnum School { get; set; }
+    public string? Description { get; set; }
+    public string? Notes { get; set; }
+    public required CourseGroupingStateEnum State { get; set; }
+    public int? Version { get; set; }
+    public required bool Published { get; set; }
+    public required ICollection<CourseGroupingDTO> SubGroupings { get; set; } = new List<CourseGroupingDTO>();
+    public required ICollection<CourseDataDTO> Courses { get; set; } = new List<CourseDataDTO>();
+    public required DateTime CreatedDate { get; set; }
+    public required DateTime ModifiedDate { get; set; }
+}
