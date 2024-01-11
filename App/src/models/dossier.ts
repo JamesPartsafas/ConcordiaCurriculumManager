6e28a160457f1f09b8a4eb30e0622d17d17f5701
rogersi9 <69977233+rogersi9@users.noreply.github.com>
2024-01-10 22:17:26 -0500

    Create DossierReport Interface #297

--- App/src/models/dossier.ts
+++ App/src/models/dossier.ts
@@ -1,5 +1,5 @@
 import { GroupDTO } from "../services/group";
-import { CourseCreationRequest, CourseDeletionRequest, CourseModificationRequest } from "./course";
+import { Course, CourseCreationRequest, CourseDeletionRequest, CourseModificationRequest } from "./course";
 
 export interface DossierDTO {
     id: string;
@@ -67,6 +67,22 @@ export enum DossierStateEnum {
     InReview = 1,
     Rejected = 2,
     Approved = 3,
+}
+
+export interface DossierReportDTO {
+    approvalStages: ApprovalStage[];
+    courseCreationRequests: CourseCreationRequest[];
+    courseModificationRequests: CourseModificationRequest[];
+    courseDeletionRequests: CourseDeletionRequest[];
+    description: string | null;
+    initiatiorId: string;
+    oldCourses: Course[];
+    state: number;
+    title: string | null;
+}
+
+export interface DossierReportResponse {
+    data: DossierReportDTO;
 }
 
 export function dossierStateToString(dossier: DossierDTO | DossierDetailsDTO | null): string {
