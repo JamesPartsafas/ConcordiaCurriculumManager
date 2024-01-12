1472c3e4c5cabb04b15d266b05c53b6eab69aed0
Marc Eid <marc.mn.eid@gmail.com>
2024-01-11 20:59:08 -0500

    fixed button margins

--- App/src/pages/ManageableGroups.tsx
+++ App/src/pages/ManageableGroups.tsx
@@ -13,6 +13,7 @@ import {
     AlertDialogHeader,
     AlertDialogContent,
     AlertDialogOverlay,
+    Box,
 } from "@chakra-ui/react";
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
@@ -55,56 +56,88 @@ export default function DisplayManageableGroups() {
     };
 
     return (
-        <div>
-            <Container maxW="3xl" height="80vh" display="flex" alignItems="center" justifyContent="center">
-                <div>
-                    <h1
-                        style={{
-                            textAlign: "center",
-                            marginBottom: "20px",
-                            fontWeight: "bold",
-                            fontSize: "24px",
-                            color: "brandRed",
-                        }}
-                    >
-                        Group Information
-                    </h1>
-                    <Table variant="simple" size="lg">
-                        <Thead>
-                            <Tr>
-                                <Th whiteSpace="nowrap">Group Name</Th>
-                                <Th whiteSpace="nowrap">Applications to Approve</Th>
-                                <Th whiteSpace="nowrap">Number of Members</Th>
-                                <Th whiteSpace="nowrap">Manage Members</Th>
-                                {isAdmin(user) && <Th whiteSpace="nowrap">Manage Masters</Th>}
-                                {isAdmin(user) && <Th whiteSpace="nowrap">Edit Group</Th>}
-                                {isAdmin(user) && <Th whiteSpace="nowrap">Delete Group</Th>}
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            {myGroups.map((group, index) => (
-                                <Tr key={index}>
-                                    <Td whiteSpace="nowrap" padding="16px" textAlign="center">
-                                        {group.name}
-                                    </Td>
-                                    <Td whiteSpace="nowrap" padding="16px" textAlign="center">
-                                        {0}
-                                    </Td>
-                                    <Td whiteSpace="nowrap" padding="16px" textAlign="center">
-                                        {group.members?.length ?? 0}
-                                    </Td>
-                                    <Td whiteSpace="nowrap" padding="16px">
+        <Container display="flex" justifyContent="center">
+            <Box>
+                <Link to={BaseRoutes.Home}>
+                    <Button style="primary" variant="outline" height="40px" mt={2}>
+                        Back to Home Page
+                    </Button>
+                </Link>
+                <h1
+                    style={{
+                        textAlign: "center",
+                        marginBottom: "25px",
+                        fontWeight: "bold",
+                        fontSize: "24px",
+                        color: "brandRed",
+                        alignSelf: "center",
+                    }}
+                >
+                    Group Information
+                </h1>
+
+                <Table variant="simple" size="md">
+                    <Thead>
+                        <Tr>
+                            <Th whiteSpace="nowrap" textAlign={"center"}>
+                                Group Name
+                            </Th>
+                            <Th whiteSpace="nowrap" textAlign={"center"}>
+                                Applications to Approve
+                            </Th>
+                            <Th whiteSpace="nowrap" textAlign={"center"}>
+                                Number of Members
+                            </Th>
+                            <Th whiteSpace="nowrap" textAlign={"center"}>
+                                Manage Members
+                            </Th>
+                            {isAdmin(user) && <Th whiteSpace="nowrap">Manage Masters</Th>}
+                            {isAdmin(user) && <Th whiteSpace="nowrap">Edit Group</Th>}
+                            {isAdmin(user) && <Th whiteSpace="nowrap">Delete Group</Th>}
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {myGroups.map((group, index) => (
+                            <Tr key={index}>
+                                <Td whiteSpace="nowrap" padding="17px" textAlign="center">
+                                    {group.name}
+                                </Td>
+                                <Td whiteSpace="nowrap" padding="16px" textAlign="center">
+                                    {0}
+                                </Td>
+                                <Td whiteSpace="nowrap" padding="16px" textAlign="center">
+                                    {group.members?.length ?? 0}
+                                </Td>
+                                <Td whiteSpace="nowrap" padding="16px">
+                                    <Link to={BaseRoutes.AddUserToGroup} state={{ gid: group.id, name: group.name }}>
+                                        <Button style="primary" variant="outline" width="50%" height="40px" mr={1}>
+                                            Add
+                                        </Button>
+                                    </Link>
+                                    {(group.members?.length || 0) !== 0 && (
                                         <Link
-                                            to={BaseRoutes.AddUserToGroup}
+                                            to={BaseRoutes.RemoveUserFromGroup}
                                             state={{ gid: group.id, name: group.name }}
                                         >
                                             <Button style="primary" variant="outline" width="50%" height="40px">
+                                                Remove
+                                            </Button>
+                                        </Link>
+                                    )}
+                                </Td>
+                                {isAdmin(user) && (
+                                    <Td whiteSpace="nowrap" padding="16px" textAlign="center">
+                                        <Link
+                                            to={BaseRoutes.AddGroupMaster}
+                                            state={{ gid: group.id, name: group.name }}
+                                        >
+                                            <Button style="primary" variant="outline" width="50%" height="40px" mr={1}>
                                                 Add
                                             </Button>
                                         </Link>
-                                        {(group.members?.length || 0) !== 0 && (
+                                        {(group.groupMasters?.length || 0) !== 0 && (
                                             <Link
-                                                to={BaseRoutes.RemoveUserFromGroup}
+                                                to={BaseRoutes.RemoveGroupMaster}
                                                 state={{ gid: group.id, name: group.name }}
                                             >
                                                 <Button style="primary" variant="outline" width="50%" height="40px">
@@ -113,100 +146,74 @@ export default function DisplayManageableGroups() {
                                             </Link>
                                         )}
                                     </Td>
-                                    {isAdmin(user) && (
-                                        <Td whiteSpace="nowrap" padding="16px" textAlign="center">
-                                            <Link
-                                                to={BaseRoutes.AddGroupMaster}
-                                                state={{ gid: group.id, name: group.name }}
+                                )}
+                                {isAdmin(user) && (
+                                    <Td whiteSpace="nowrap" padding="16px" textAlign="center">
+                                        <Link to={BaseRoutes.ManageableGroup}>
+                                            <Button style="primary" variant="outline" width="50%" height="40px">
+                                                Edit {/**for Future Edit Group Name */}
+                                            </Button>
+                                        </Link>
+                                    </Td>
+                                )}
+                                {isAdmin(user) && (
+                                    <Td whiteSpace="nowrap" padding="16px" textAlign="center">
+                                        <Link to={BaseRoutes.ManageableGroup}>
+                                            <Button
+                                                style="primary"
+                                                variant="outline"
+                                                width="50%"
+                                                height="40px"
+                                                onClick={() => setDeleteGroupId(group.id)}
                                             >
-                                                <Button style="primary" variant="outline" width="50%" height="40px">
-                                                    Add
-                                                </Button>
-                                            </Link>
-                                            {(group.groupMasters?.length || 0) !== 0 && (
-                                                <Link
-                                                    to={BaseRoutes.RemoveGroupMaster}
-                                                    state={{ gid: group.id, name: group.name }}
-                                                >
-                                                    <Button style="primary" variant="outline" width="50%" height="40px">
-                                                        Remove
-                                                    </Button>
-                                                </Link>
-                                            )}
-                                        </Td>
-                                    )}
-                                    {isAdmin(user) && (
-                                        <Td whiteSpace="nowrap" padding="16px" textAlign="center">
-                                            <Link to={BaseRoutes.ManageableGroup}>
-                                                <Button style="primary" variant="outline" width="50%" height="40px">
-                                                    Edit {/**for Future Edit Group Name */}
-                                                </Button>
-                                            </Link>
-                                        </Td>
-                                    )}
-                                    {isAdmin(user) && (
-                                        <Td whiteSpace="nowrap" padding="16px" textAlign="center">
-                                            <Link to={BaseRoutes.ManageableGroup}>
-                                                <Button
-                                                    style="primary"
-                                                    variant="outline"
-                                                    width="50%"
-                                                    height="40px"
-                                                    onClick={() => setDeleteGroupId(group.id)}
-                                                >
-                                                    Delete {/**for Future Edit Group Name */}
-                                                </Button>
-                                            </Link>
-                                        </Td>
-                                    )}
-                                </Tr>
-                            ))}
-                        </Tbody>
-                    </Table>
-                    {isAdmin(user) && (
-                        <Link to={BaseRoutes.CreateGroup}>
-                            <Button style="primary" variant="solid" width="100%" height="40px">
-                                Create Group
-                            </Button>
-                        </Link>
-                    )}
-                    <Link to={BaseRoutes.Home}>
+                                                Delete {/**for Future Edit Group Name */}
+                                            </Button>
+                                        </Link>
+                                    </Td>
+                                )}
+                            </Tr>
+                        ))}
+                    </Tbody>
+                </Table>
+
+                {isAdmin(user) && (
+                    <Link to={BaseRoutes.CreateGroup}>
                         <Button style="primary" variant="solid" width="100%" height="40px">
-                            Back to Home Page
+                            Create Group
                         </Button>
                     </Link>
-                    <AlertDialog
-                        isOpen={deleteGroupId !== null}
-                        leastDestructiveRef={cancelRef}
-                        onClose={() => setDeleteGroupId(null)}
-                    >
-                        <AlertDialogOverlay>
-                            <AlertDialogContent>
-                                <AlertDialogHeader fontSize="lg" fontWeight="bold">
-                                    Delete Group
-                                </AlertDialogHeader>
+                )}
+                <AlertDialog
+                    isOpen={deleteGroupId !== null}
+                    leastDestructiveRef={cancelRef}
+                    onClose={() => setDeleteGroupId(null)}
+                >
+                    <AlertDialogOverlay>
+                        <AlertDialogContent>
+                            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                                Delete Group
+                            </AlertDialogHeader>
 
-                                <AlertDialogBody>Are you sure? This action cannot be undone.</AlertDialogBody>
+                            <AlertDialogBody>Are you sure? This action cannot be undone.</AlertDialogBody>
 
-                                <AlertDialogFooter>
-                                    <Button ref={cancelRef} onClick={() => setDeleteGroupId(null)}>
-                                        Cancel
-                                    </Button>
-                                    <Button
-                                        colorScheme="red"
-                                        onClick={() => {
-                                            onDeleteGroup(deleteGroupId);
-                                        }}
-                                        ml={3}
-                                    >
-                                        Delete
-                                    </Button>
-                                </AlertDialogFooter>
-                            </AlertDialogContent>
-                        </AlertDialogOverlay>
-                    </AlertDialog>
-                </div>
-            </Container>
-        </div>
+                            <AlertDialogFooter>
+                                <Button ref={cancelRef} onClick={() => setDeleteGroupId(null)}>
+                                    Cancel
+                                </Button>
+                                <Button
+                                    colorScheme="red"
+                                    onClick={() => {
+                                        onDeleteGroup(deleteGroupId);
+                                    }}
+                                    ml={3}
+                                >
+                                    Delete
+                                </Button>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialogOverlay>
+                </AlertDialog>
+            </Box>
+        </Container>
     );
 }
