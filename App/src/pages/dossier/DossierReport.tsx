f92b8a5fee2883c9bebb81cbed228866dbf0ef5e
rogersi9 <69977233+rogersi9@users.noreply.github.com>
2024-01-17 12:54:55 -0500

    Make a loop to iterate over courses #297

--- App/src/pages/dossier/DossierReport.tsx
+++ App/src/pages/dossier/DossierReport.tsx
@@ -1,10 +1,27 @@
 import { useEffect, useState } from "react";
 import { getDossierReport } from "../../services/dossier";
 import { DossierReportDTO } from "../../models/dossier";
-import { Heading, Center, Text, Card, CardHeader, CardBody, SimpleGrid, CardFooter, Button } from "@chakra-ui/react";
+import {
+    Heading,
+    Center,
+    Text,
+    Card,
+    CardHeader,
+    CardBody,
+    SimpleGrid,
+    CardFooter,
+    Button,
+    Box,
+} from "@chakra-ui/react";
 
 export default function DossierReport() {
     const [dossierReport, setDossierReport] = useState<DossierReportDTO | null>(null);
+    const items = [
+        { title: "Course Creation Requests:", key: "courseCreationRequests" },
+        { title: "Course Deletion Requests:", key: "courseDeletionRequests" },
+        { title: "Course Modification Requests:", key: "courseModificationRequests" },
+    ];
+
     useEffect(() => {
         getDossierReport("37581d9d-713f-475c-9668-23971b0e64d0")
             .then((res) => {
@@ -27,28 +44,35 @@ export default function DossierReport() {
                             {dossierReport.description}
                         </Text>
                     </Center>
-                    <Heading p={4}>Course Creation Requests:</Heading>
-                    <SimpleGrid spacing={4} templateColumns="repeat(auto-fill, minmax(200px, 1fr))">
-                        {dossierReport.courseCreationRequests.map((el, index) => {
-                            return (
-                                <Card key={index}>
-                                    <CardHeader>
-                                        <Heading size="md">
-                                            {el.newCourse.subject} {el.newCourse.catalog}
-                                        </Heading>
-                                    </CardHeader>
-                                    <CardBody py={2}>
-                                        <Text>
-                                            <b>Comment:</b> {el.comment ? el.comment : "No comment"}
-                                        </Text>
-                                    </CardBody>
-                                    <CardFooter>
-                                        <Button>View</Button>
-                                    </CardFooter>
-                                </Card>
-                            );
-                        })}
-                    </SimpleGrid>
+                    {items.map((el, index) => {
+                        return (
+                            <Box key={index}>
+                                <Heading p={4}>{el.title}</Heading>
+                                <SimpleGrid spacing={4} templateColumns="repeat(auto-fill, minmax(200px, 1fr))">
+                                    {dossierReport[el.key].map((el, index) => {
+                                        return (
+                                            <Card key={index}>
+                                                <CardHeader>
+                                                    <Heading size="md">
+                                                        {el.newCourse ? el.newCourse.subject : el.course.subject}{" "}
+                                                        {el.newCourse ? el.newCourse.catalog : el.course.catalog}
+                                                    </Heading>
+                                                </CardHeader>
+                                                <CardBody py={2}>
+                                                    <Text>
+                                                        <b>Comment:</b> {el.comment ? el.comment : "No comment"}
+                                                    </Text>
+                                                </CardBody>
+                                                <CardFooter>
+                                                    <Button>View</Button>
+                                                </CardFooter>
+                                            </Card>
+                                        );
+                                    })}
+                                </SimpleGrid>
+                            </Box>
+                        );
+                    })}
                 </>
             ) : null}
         </>
