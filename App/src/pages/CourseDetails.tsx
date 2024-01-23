3b961177cc0f790bba03c326b1bf655a63c8d76a
Agnes Croteau <agnes.croteau@gmail.com>
2024-01-22 23:21:06 -0500

    Fetch course details data from backend

--- App/src/pages/CourseDetails.tsx
+++ App/src/pages/CourseDetails.tsx
@@ -1,65 +1,65 @@
-import React, { useEffect, useState } from "react";
+import { 
+    useToast, Card, Text, Heading, 
+} from "@chakra-ui/react";
+import { 
+    useEffect, 
+    useState 
+} from "react";
 import { useLocation } from "react-router-dom";
-import { Card, Heading, Text } from "@chakra-ui/react";
+import { Course } from "../models/course";
+import { getCourseData} from "../services/course";
+import { showToast } from "../utils/toastUtils";
 
 function useQuery() {
     return new URLSearchParams(useLocation().search);
 }
 
-const CourseDetails = () => {
+export default function CourseDetails() {
     const query = useQuery(); // Use the useQuery function
-
+    const [courseData, setCourseData] = useState<Course>(null);
+    
     // Access specific query parameters
     const subject: string = query.get("subject");
-    const catalogNumber: string = query.get("catalog");
-    const [data, setData] = useState([]);
-
+    const catalogNumber: number = Number(query.get("catalog"));
+    
+    const toast = useToast();
+    
     useEffect(() => {
-        fetch("/data.json")
-            .then((response) => response.json())
-            .then((json) => {
-                if (catalogNumber) {
-                    const result = json?.Data?.filter((d) => d?.Subject == subject && d?.Catalog == catalogNumber);
-                    setData(result);
-                } else {
-                    const result = json?.Data?.filter((d) => d?.Subject == subject);
-                    setData(result);
-                }
-            });
-    }, [subject, catalogNumber]);
-    console.log(subject, catalogNumber);
-    return (
-        <div style={{ maxWidth: "90%", marginLeft: "5%", marginRight: "5%", paddingTop: "30px" }}>
-            {data?.map((d) => (
-                <Card key={d?.Id} marginBottom={20} padding={15}>
+        getCourseData(subject, catalogNumber)
+        .then((res) => {
+            setCourseData(res.data);
+        })
+        .catch((err) => {
+            showToast(toast, "Error!", err, "error");
+        })
+    }, []);
+
+
+    return ( 
+          <div style={{ maxWidth: "50%", marginLeft: "25%", marginRight: "25%", paddingTop: "100px" }}>
+          
+                <Card key={courseData?.courseID} marginBottom={20} padding={15}>
                     <Heading as="h1" size="md" marginBottom={8}>
-                        {d?.Subject} {d?.Catalog} {d?.Title} ({parseInt(d?.CreditValue)} credits)
-                    </Heading>
+                       {courseData?.subject} {courseData?.catalog} {courseData?.title} ({parseInt(courseData?.creditValue)} credits)
+                   </Heading>
                     <Text marginBottom={5}>
                         <Text as="b" marginRight={2}>
                             Prerequisite/Corequisite:
                         </Text>{" "}
                         Permission of the GCS is required.{" "}
-                    </Text>
-                    <Text marginBottom={5}>
-                        <Text as="b" marginRight={2}>
-                            Description:
-                        </Text>{" "}
-                        {d?.Description}{" "}
-                    </Text>
-
-                    <Text>
-                        <Text as="b" marginRight={2}>
-                            {" "}
-                            Component(s):
                         </Text>
-                        {d?.CourseComponent?.slice(0, -1)?.map((c) => <span key={c?.Id}>{c?.ComponentName},</span>)}
-                        {d?.CourseComponent[d?.CourseComponent?.length - 1]?.ComponentName}
-                    </Text>
-                </Card>
-            ))}
-        </div>
-    );
-};
+                    <Text marginBottom={5}>
+                   <Text as="b" marginRight={2}>
+                  Description:
+                    </Text>{" "}
+                       {courseData?.description}{" "}
+                     </Text>
 
-export default CourseDetails;
+             
+                </Card>
+          
+          </div>
+       
+       
+    );
+ }
