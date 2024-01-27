192e4d2cde089e8d135d6c4345225dd495af02b8
Marc Eid <marc.mn.eid@gmail.com>
2024-01-27 16:31:47 -0500

    displayed top level lists

--- App/src/pages/CourseGrouping.tsx
+++ App/src/pages/CourseGrouping.tsx
@@ -1,10 +1,9 @@
-import { get } from "http";
 import { useEffect, useState } from "react";
 import { GetCourseGrouping } from "../services/courseGrouping";
 import { CourseGroupingDTO } from "../models/courseGrouping";
-import { set } from "react-hook-form";
+
 import { useParams } from "react-router-dom";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, ListItem, Text, UnorderedList } from "@chakra-ui/react";
 
 export default function CourseGrouping() {
     const [courseGrouping, setCourseGrouping] = useState<CourseGroupingDTO>();
@@ -33,8 +32,58 @@ export default function CourseGrouping() {
 
     return (
         <>
-            <Box mt={10} backgroundColor={"brandRed"} height={"100px"} display={"flex"} alignItems={"center"} justifyContent={"center"}>
-                <Heading textAlign={"center"} color={"white"}>Degree Requirements for {courseGrouping?.name}</Heading>
+            <Box
+                mt={10}
+                backgroundColor={"brandRed"}
+                minH={"100px"}
+                display="flex"
+                flexDirection={"row"}
+                alignItems="center"
+            >
+                <Box w={"70%"} margin={"auto"} py={3}>
+                    <Heading size={"3xl"} color={"white"}>
+                        Degree Requirements for {courseGrouping?.name}
+                    </Heading>
+                </Box>
+            </Box>
+
+            <Box w={"70%"} margin={"auto"}>
+                <Heading size={"2xl"} mb={3} mt={10}>
+                    Degree Requirements
+                </Heading>
+                <Text>{courseGrouping?.description}</Text>
+
+                <Heading mb={3} mt={10} color={"brandRed"}>
+                    {courseGrouping?.name} ({parseFloat(courseGrouping?.requiredCredits)} credits)
+                </Heading>
+
+                {courseGrouping?.subGroupings.map((subGrouping, index) => (
+                    <Text ml={2} mb={3} key={index}>
+                        {parseFloat(subGrouping.requiredCredits).toString().padEnd(5, "\u00A0")} credits from the{" "}
+                        {subGrouping.name} <br />
+                    </Text>
+                ))}
+
+                {courseGrouping?.subGroupings.map((subGrouping, index) => (
+                    <Box ml={2} mb={5}>
+                        <Heading color={"brandRed"} mb={3} key={index}>
+                            {subGrouping.name} ({parseFloat(subGrouping.requiredCredits)} credits)
+                        </Heading>
+
+                        <Text>{subGrouping?.description}</Text>
+
+                        <UnorderedList ml={12}>
+                            {subGrouping.courses.map((course, index) => (
+                                <ListItem key={index}>
+                                    <Text mb={2}>
+                                        {course.subject + " " + course.catalog + " " + course.title} (
+                                        {parseFloat(course.creditValue)})
+                                    </Text>
+                                </ListItem>
+                            ))}
+                        </UnorderedList>
+                    </Box>
+                ))}
             </Box>
         </>
     );
