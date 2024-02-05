7e6b75dc7772d3f2ef68ec029938dad7d1c2ad3e
DanielSoldera <d.v.soldera@gmail.com>
2024-02-05 15:12:48 -0500

    Create temporary page for frontend

---
+++ App/src/pages/CourseGroupingBySchool.tsx
@@ -0,0 +1,10 @@
+import { Text } from "@chakra-ui/react";
+export default function groupingBySchool() {
+    return (
+        <div>
+            <Text textAlign="center" fontSize="3xl" fontWeight="bold" marginTop="7%" marginBottom="5">
+                Curriculum by School
+            </Text>
+        </div>
+    );
+}
