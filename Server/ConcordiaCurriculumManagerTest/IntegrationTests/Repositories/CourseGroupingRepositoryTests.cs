d388f3c815572abe4d745dfd662261568044beee
Ghaith Chrit <ghaithfinadnan@gmail.com>
2024-02-22 20:58:41 -0500

    #439 Change test cases to expect only latest accepted version

--- Server/ConcordiaCurriculumManagerTest/IntegrationTests/Repositories/CourseGroupingRepositoryTests.cs
+++ Server/ConcordiaCurriculumManagerTest/IntegrationTests/Repositories/CourseGroupingRepositoryTests.cs
@@ -31,55 +31,92 @@ public class CourseGroupingRepositoryTests
     {
         var firstGrouping = TestData.GetSampleCourseGrouping();
         var secondGrouping = TestData.GetSampleCourseGrouping();
+        var thirdGrouping = TestData.GetSampleCourseGrouping();
+
+        secondGrouping.CommonIdentifier = firstGrouping.CommonIdentifier;
+        thirdGrouping.CommonIdentifier = firstGrouping.CommonIdentifier;
 
         firstGrouping.School = SchoolEnum.GinaCody;
         secondGrouping.School = SchoolEnum.GinaCody;
+        thirdGrouping.School = SchoolEnum.GinaCody;
 
         firstGrouping.Version = 1;
         secondGrouping.Version = null;
+        thirdGrouping.Version = 2;
 
         firstGrouping.State = CourseGroupingStateEnum.Accepted;
         secondGrouping.State = CourseGroupingStateEnum.CourseGroupingDeletionProposal;
+        thirdGrouping.State = CourseGroupingStateEnum.Accepted;
 
-        firstGrouping.Published = true;
+        firstGrouping.Published = false;
         secondGrouping.Published = false;
+        thirdGrouping.Published = true;
 
-        var groupings = new List<CourseGrouping> { firstGrouping, secondGrouping };
+        var groupings = new List<CourseGrouping> { firstGrouping, secondGrouping, thirdGrouping };
 
         await dbContext.AddRangeAsync(groupings);
         await dbContext.SaveChangesAsync();
         var result = await courseGroupingRepository.GetCourseGroupingsBySchool(SchoolEnum.GinaCody);
 
         Assert.AreEqual(result.Count, 1);
+        Assert.AreEqual(result.First().Id, thirdGrouping.Id);
     }
 
     [TestMethod]
     public async Task GetCourseGroupingsLikeName_WithValidSchoolName_ReturnsOnlyAcceptedCourseGroupings()
     {
-        var firstGrouping = TestData.GetSampleCourseGrouping();
-        var secondGrouping = TestData.GetSampleCourseGrouping();
+        var firstCourseFirstGrouping = TestData.GetSampleCourseGrouping();
+        var secondCourseFirstGrouping = TestData.GetSampleCourseGrouping();
 
-        firstGrouping.Name = "This is a name";
-        secondGrouping.Name = "This is a name";
+        var firstCourseSecondGrouping = TestData.GetSampleCourseGrouping();
+        var secondCourseSecondGrouping = TestData.GetSampleCourseGrouping();
 
-        firstGrouping.School = SchoolEnum.GinaCody;
-        secondGrouping.School = SchoolEnum.GinaCody;
+        firstCourseFirstGrouping.CommonIdentifier = secondCourseFirstGrouping.CommonIdentifier;
+        firstCourseSecondGrouping.CommonIdentifier = secondCourseSecondGrouping.CommonIdentifier;
 
-        firstGrouping.Version = 1;
-        secondGrouping.Version = null;
+        firstCourseFirstGrouping.Name = "This is a name1";
+        secondCourseFirstGrouping.Name = "This is a name1";
+        firstCourseSecondGrouping.Name = "This is a name2";
+        secondCourseSecondGrouping.Name = "This is a name2";
 
-        firstGrouping.State = CourseGroupingStateEnum.Accepted;
-        secondGrouping.State = CourseGroupingStateEnum.CourseGroupingDeletionProposal;
+        firstCourseFirstGrouping.School = SchoolEnum.GinaCody;
+        secondCourseFirstGrouping.School = SchoolEnum.GinaCody;
+        firstCourseSecondGrouping.School = SchoolEnum.GinaCody;
+        secondCourseSecondGrouping.School = SchoolEnum.GinaCody;
 
-        firstGrouping.Published = true;
-        secondGrouping.Published = false;
+        firstCourseFirstGrouping.Version = 1;
+        secondCourseFirstGrouping.Version = 2;
+        firstCourseSecondGrouping.Version = 1;
+        secondCourseSecondGrouping.Version = null;
 
-        var groupings = new List<CourseGrouping> { firstGrouping, secondGrouping };
+        firstCourseFirstGrouping.State = CourseGroupingStateEnum.Accepted;
+        secondCourseFirstGrouping.State = CourseGroupingStateEnum.Accepted;
+        firstCourseSecondGrouping.State = CourseGroupingStateEnum.Accepted;
+        secondCourseSecondGrouping.State = CourseGroupingStateEnum.Accepted;
+
+        firstCourseFirstGrouping.Published = false;
+        secondCourseFirstGrouping.Published = true;
+        firstCourseSecondGrouping.Published = true;
+        secondCourseSecondGrouping.Published = false;
+
+        var groupings = new List<CourseGrouping> { firstCourseFirstGrouping, secondCourseFirstGrouping, firstCourseSecondGrouping, secondCourseSecondGrouping };
 
         await dbContext.AddRangeAsync(groupings);
         await dbContext.SaveChangesAsync();
         var result = await courseGroupingRepository.GetCourseGroupingsLikeName("  This is a  ");
 
-        Assert.AreEqual(result.Count, 1);
+        Assert.AreEqual(result.Count, 2);
+
+        foreach (var courseGrouping in result)
+        {
+            if (courseGrouping.CommonIdentifier.Equals(firstCourseSecondGrouping.CommonIdentifier))
+            {
+                Assert.AreEqual(firstCourseSecondGrouping.Id, courseGrouping.Id);
+            }
+            else
+            {
+                Assert.AreEqual(secondCourseFirstGrouping.Id, courseGrouping.Id);
+            }
+        }
     }
 }
