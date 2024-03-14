9d402e90e1ea6988c4daa607c1a28060de6cdc2e
Marc Eid <marc.mn.eid@gmail.com>
2024-03-13 22:39:11 -0400

    Added Heading to show the program selected.

--- App/src/pages/CoursesLeft.tsx
+++ App/src/pages/CoursesLeft.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Button from "../components/Button";
-import { Box, useDisclosure } from "@chakra-ui/react";
+import { Box, Heading, useDisclosure } from "@chakra-ui/react";
 import SearchCourseGrouping from "../components/CourseGrouping/SearchCourseGrouping";
 import { GetCourseGrouping } from "../services/courseGrouping";
 
@@ -35,7 +35,9 @@ export default function CoursesLeft() {
             .then(
                 (response) => {
                     console.log(response.data);
-                    setCourseGrouping(response.data);
+                    if (response.data.isTopLevel) {
+                        setCourseGrouping(response.data);
+                    }
                 },
                 (rej) => {
                     console.log(rej);
@@ -63,6 +65,24 @@ export default function CoursesLeft() {
                 >
                     Select Program
                 </Button>
+            </Box>
+
+            <Box
+                mt={10}
+                mb={10}
+                backgroundColor={"brandRed"}
+                minH={"100px"}
+                display="flex"
+                flexDirection={"row"}
+                alignItems="center"
+            >
+                <Box w={"70%"} margin={"auto"} py={3}>
+                    <Heading size={"3xl"} color={"white"}>
+                        {courseGrouping?.isTopLevel
+                            ? " Degree Requirements for " + courseGrouping?.name
+                            : "Select a Program"}
+                    </Heading>
+                </Box>
             </Box>
         </>
     );
