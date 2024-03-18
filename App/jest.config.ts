f9cb0247e0d58b66be122b33411593a31aec12d7
rogersi9 <69977233+rogersi9@users.noreply.github.com>
2024-03-17 23:15:55 -0400

    fix frontend tests

--- App/jest.config.ts
+++ App/jest.config.ts
@@ -3,21 +3,19 @@ export default {
     testEnvironment: "jest-environment-jsdom",
     testMatch: ["**/***.test.tsx"],
     moduleFileExtensions: ["js", "jsx", "tsx", "ts"],
-    rootDir: "__tests__",
+    rootDir: "__tests__/",
     transform: {
         "^.+\\.tsx?$": [
             "ts-jest",
             {
                 tsconfig: "./tsconfig-test.json",
                 diagnostics: {
-                    ignoreCodes: ["TS151001"],
+                    exclude: ["!*/.test.ts?(x)"],
                 },
             },
         ],
-
         "^.+\\.jsx?$": "babel-jest",
     },
-    transformIgnorePatterns: ["node_modules/(?!(d3-selection|another-module-you-need-to-transform)/)"],
     moduleNameMapper: {
         "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/__mocks__/image_mock.tsx",
         "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
