c30ebcdbb066fd76c2ccb46ae16226b145217c3f
Ghaith Chrit <ghaithfinadnan@gmail.com>
2024-03-18 23:53:55 -0400

    #526 Add DossierMetric models

---
+++ Server/ConcordiaCurriculumManager/Models/Metrics/UserDossierViewedCount.cs
@@ -0,0 +1,10 @@
+﻿using ConcordiaCurriculumManager.Models.Users;
+
+namespace ConcordiaCurriculumManager.Models.Metrics;
+
+public class UserDossierViewedCount
+{
+    public required Guid UserId { get; set; }
+    public User? User { get; set; }
+    public required int Count { get; set; }
+}
