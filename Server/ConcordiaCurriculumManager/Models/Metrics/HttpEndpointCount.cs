90e62c3349d6256df7eb5b14b7e7f9fb59817fd9
Ghaith Chrit <ghaithfinadnan@gmail.com>
2024-03-18 21:53:12 -0400

    #524 Add HttpMetric Models

---
+++ Server/ConcordiaCurriculumManager/Models/Metrics/HttpEndpointCount.cs
@@ -0,0 +1,9 @@
+﻿namespace ConcordiaCurriculumManager.Models.Metrics;
+
+public class HttpEndpointCount
+{
+    public required string Controller { get; set; }
+    public required string Endpoint { get; set; }
+    public required string FullPath { get; set; }
+    public required int Count { get; set; }
+}
---
+++ Server/ConcordiaCurriculumManager/Models/Metrics/HttpMetrics.cs
@@ -0,0 +1,9 @@
+﻿namespace ConcordiaCurriculumManager.Models.Metrics;
+
+public class HttpMetric : BaseModel
+{
+    public required string Controller { get; set; }
+    public required string Endpoint { get; set; }
+    public required int ResponseStatusCode { get; set; }
+    public required long ResponseTimeMilliSecond { get; set; }
+}
