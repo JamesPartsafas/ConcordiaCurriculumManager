950043deaafdb0a00d710fd53f68de128b52c625
JamesThePlant <56362880+JamesThePlant@users.noreply.github.com>
2024-03-23 15:26:20 -0400

    #543 Create migration for message deletion

---
+++ Server/ConcordiaCurriculumManager/Migrations/20240323183957_AllowMessageDeletion.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConcordiaCurriculumManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AllowMessageDeletion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "DiscussionMessage",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "DiscussionMessage");
+        }
+    }
+}
