563aba89dc6038e8db8676ac84efd3ff5c89f4a2
Ghaith Chrit <ghaithfinadnan@gmail.com>
2024-03-23 23:22:22 -0400

    #500 Add Vote Tables

---
+++ Server/ConcordiaCurriculumManager/Migrations/20240324031419_DossierMessageVote.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConcordiaCurriculumManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class DossierMessageVote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "VoteCount",
+                table: "DiscussionMessage",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "DiscussionMessageVote",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    DiscussionMessageId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    DiscussionMessageVoteValue = table.Column<int>(type: "integer", nullable: false),
+                    UserId1 = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiscussionMessageVote", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DiscussionMessageVote_DiscussionMessage_DiscussionMessageId",
+                        column: x => x.DiscussionMessageId,
+                        principalTable: "DiscussionMessage",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DiscussionMessageVote_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DiscussionMessageVote_Users_UserId1",
+                        column: x => x.UserId1,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DiscussionMessageVote_DiscussionMessageId",
+                table: "DiscussionMessageVote",
+                column: "DiscussionMessageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DiscussionMessageVote_UserId_DiscussionMessageId",
+                table: "DiscussionMessageVote",
+                columns: new[] { "UserId", "DiscussionMessageId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DiscussionMessageVote_UserId1",
+                table: "DiscussionMessageVote",
+                column: "UserId1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiscussionMessageVote");
+
+            migrationBuilder.DropColumn(
+                name: "VoteCount",
+                table: "DiscussionMessage");
+        }
+    }
+}
