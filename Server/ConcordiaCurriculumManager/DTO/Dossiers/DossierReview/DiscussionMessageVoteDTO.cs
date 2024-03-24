97b295ec8d8e84766a26511a7f73e03c7358cb1d
Ghaith Chrit <ghaithfinadnan@gmail.com>
2024-03-23 23:40:01 -0400

    #500 Return vote count on dossier details

---
+++ Server/ConcordiaCurriculumManager/DTO/Dossiers/DossierReview/DiscussionMessageVoteDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ConcordiaCurriculumManager.DTO.Dossiers.DossierReview;
+
+public class DiscussionMessageVoteDTO
+{
+    public required Guid DiscussionMessageId { get; set; }
+
+    public required Guid UserId { get; set; }
+
+    public required VoteDossierDiscussionMessageValue DiscussionMessageVoteValue { get; set; }
+}
--- Server/ConcordiaCurriculumManager/DTO/Dossiers/DossierReview/DossierDiscussionMessageDTO.cs
+++ Server/ConcordiaCurriculumManager/DTO/Dossiers/DossierReview/DossierDiscussionMessageDTO.cs
@@ -9,4 +9,6 @@ public class DossierDiscussionMessageDTO
     public Guid? ParentDiscussionMessageId { get; set; }
     public required DateTime CreatedDate { get; set; }
     public required DateTime ModifiedDate { get; set; }
+    public int VoteCount { get; set; } = 0;
+    public IEnumerable<DiscussionMessageVoteDTO> DiscussionMessageVotes { get; set; } = new List<DiscussionMessageVoteDTO>();
 }
